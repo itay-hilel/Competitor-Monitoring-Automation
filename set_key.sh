fa426ebee97adabf84ec7da9aa828ae39e2f8b71
Itay Hilel <itay@varos.com>
2025-11-27 01:23:45 +0200

    Remove unused files related to JWKS generation, key setting, and architecture documentation.

--- set_key.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Read the key from file
-KEY=$(cat private_key_pkcs8.pem)
-# Set the environment variable using convex env set
-# We wrap the key in quotes to handle newlines properly
-npx convex env set JWT_PRIVATE_KEY "$KEY"
-
